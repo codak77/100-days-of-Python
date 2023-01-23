4c683e5d4ff7be837b8eaf37443f2d397d5ca79f
Sean Stevens <seanchi77@gmail.com>
2023-01-23 15:09:03 +0100

    Create day-1-main.py

---
+++ day-1-main.py
@@ -0,0 +1,11 @@
+# Write your code below this line 👇
+print("Hello world!\nHello world!")
+print("Hello" + " " + "Angela")
+# print("Hello" + " " + input("what's your name?\n") + "!")
+#input("what's your name?\n")
+#print( len(input("what's your name?\n")) )
+"""
+name = input("what's your name?\n")
+length = len(name)
+print(length)
+"""
